2125701aea80880336e97e3baeb3fbff7d765936
Joseph Mellin <mellin.joe@gmail.com>
2015-05-15 00:55:26 -0700

    add lotta

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -18,7 +18,7 @@ class TwilioController < ApplicationController
         r.Say 'You selected Nietos story'
         r.Play 'https://s3.amazonaws.com/uploads.getnowapp.co/Nietos1.mp3'
       elsif params[:Digits].to_i == 2 
-        r.Say 'You selected Toms story'
+        r.Say 'You selected Tom and Patricias story'
         r.Play 'https://s3.amazonaws.com/uploads.getnowapp.co/TomP2.mp3'
       elsif params[:Digits].to_i == 3
         r.Say 'You selected Claudias story'
@@ -37,7 +37,7 @@ class TwilioController < ApplicationController
         r.Play 'https://s3.amazonaws.com/uploads.getnowapp.co/BenitoS7.mp3'
       elsif params[:Digits].to_i == 8
         r.Say 'You selected Martinas story'
-        r.Play 'https://s3.amazonaws.com/uploads.getnowapp.co/MartinaA8.mp3'
+        r.Play 'https://s3.amazonaws.com/uploads.getnowapp.co/Lotta8.mp3'
       elsif params[:Digits].to_i == 9
         r.Say 'You selected Rios story'
         r.Play 'https://s3.amazonaws.com/uploads.getnowapp.co/Rio9.mp3'
@@ -62,7 +62,7 @@ class TwilioController < ApplicationController
     r.Gather :timeout => 10, :numDigits => 1, :finishOnKey => '*', :action => receive_main_decision_tree_twilio_path do
       options = []
       options << 'Nietos'
-      options += ['Toms', 'Claudias','Pauls', 'Lisas','Renitas','Benitos','Martinas','Rios']
+      options += ['Tom and Patricias', 'Claudias','Pauls', 'Lisas','Renitas','Benitos','Lottas','Rios']
       c = 1
       options.each do |o|
         r.Say "Press #{c} to listen to #{o} story", :voice => 'man'
